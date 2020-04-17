fb9e4ad01d987b12bd0d3711c6822472f3569c83
poojakothari89 <63854463+poojakothari89@users.noreply.github.com>
2020-04-17 18:33:37 +0530

    adding more changes to create and resolve conflict

--- src/org/jaxb/example/Test.java
+++ src/org/jaxb/example/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("changes done here...");
+		// adding changes in branch to generate conflict
+		System.out.println("branch changes done here...");
 
 	}
 

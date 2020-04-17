09a44466608959630ecb2bf47309e4a1d9f7cc5a
poojakothari89 <63854463+poojakothari89@users.noreply.github.com>
2020-04-17 18:32:42 +0530

    adding more changes to create merge conflict

--- src/org/jaxb/example/Test.java
+++ src/org/jaxb/example/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("changes done here...");
+		//adding another sysout to raised merge conflict
+		System.out.println("More changes done here...");
 
 	}
 
